139894cdd3424f61dad7cc208eecc833f9899349
Christopher Weems <hello@christopherweems.com>
2026-01-14 18:47:10 -0500

    Add `testShell()`

--- Sources/FixieTests/FixieTests.swift
+++ Sources/FixieTests/FixieTests.swift
@@ -4,7 +4,21 @@
 // Copyright (c) 2025 Christopher Weems
 // SPDX-License-Identifier: MIT
 
+import FixieCore
 import Testing
 
 @Test
-func testFixie() async throws { }
+func testShell() async throws {
+    let shell = try Shell(failFast: true)
+    
+    let _result = try shell.run("echo s3://123")
+    
+    var result = ""
+    
+    for try await r in _result {
+        result.append(String(data: r, encoding: .utf8)!)
+    }
+    
+    #expect(result.hasPrefix("s3://123"))
+    
+}
